d33808c087210c90a05c1c50a5be7a32688677fd
Alex-Kuzmenko1965 <gc-dn@ukr.net>
2023-07-02 23:22:32 +0300

    Alex

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,32 @@
+// import cl from './ContactList.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteContact } from 'redux/contactsSlice';
+import { getContacts, getFilter } from '../../redux/selectors';
+
+export const ContactList = () => {
+  const contacts = useSelector(getContacts);
+  // console.log(contacts);
+  const filter = useSelector(getFilter);
+  // console.log(filter);
+  const dispatch = useDispatch();
+
+  const filterContacts = () => {
+    if (filter === '') {return contacts;}
+    else {
+      return contacts.filter(contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase())
+      );
+    }
+  };
+
+  const getFilterContacts = filterContacts();
+
+  return (
+    <ul>
+    {getFilterContacts.map(({ id, name, number }) => (
+    <li key = {id}>{name}: {number}
+    <button type = "button" onClick = {() => dispatch(deleteContact(id))}>Delete</button>
+    </li>))}
+    </ul>
+  );
+}
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,22 @@
+// import cl from './Section.module.css';
+import { useDispatch } from 'react-redux';
+import { setFilterContact } from '../../redux/filterSlice';
+
+export const Filter = () => {
+  const dispatch = useDispatch(setFilterContact);
+
+  const handleChange = event => {
+    dispatch(setFilterContact(event.currentTarget.value));
+  };
+
+  return (
+    <label>
+      Find contacts by name   
+      <input
+      type="text"
+      name="search"      
+      onChange={handleChange}          
+      />      
+    </label>
+  );
+};
