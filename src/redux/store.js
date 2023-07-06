5a65a5b19986d022264343a4b01b952724cf9b0d
Alex-Kuzmenko1965 <gc-dn@ukr.net>
2023-07-06 19:31:39 +0300

    Alex

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //  (за замовчуванням локальне сховище для Інтернету)
+
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filterSlice";
 
-export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filter: filterReducer,
-  },
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer
 });
-// console.log(store);
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+}
+
+// Додавання змін в LocalStorage
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+console.log(store);
+
+export const persistor = persistStore(store);
